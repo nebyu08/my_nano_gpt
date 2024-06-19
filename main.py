55e06f8ae2ba0c397b9711cf43cff2173deffc66
nebiyu youhannes <nebiyuyouhannes08@gmail.com>
2024-06-19 22:54:29 +0300

    added load from pretrained

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ class GPTConfig:
 class CasualSelfAttention(nn.Module):
     def __init_(self,config):
         super().__init__()
+        assert config.n_emb//config.n_head==0,"problem with the way shape of the n_emb and n_head couldn't be divided"
         self.c_attn=nn.Linear(config.n_embd,3*config.n_embd)
         self.c_proj=nn.Linear(config.n_embd,config.n_embd)
         
@@ -49,6 +50,69 @@ class CasualSelfAttention(nn.Module):
         #the output projection becomes
         y=self.c_proj(y)
         return y
+
+    def from_pretrained(model_type):
+        """this loads model weights from pretrained hugging face 
+
+        Args:
+            model_type (string): tell us about the type of model to load from hugging face
+        """
+        assert model_type in ["model-2","minimal-v","medium versio","large version"]
+        
+        #lets get the hyperparameters for the specified model
+        model_config= {
+            'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+            'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
+            'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
+            'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
+        }[model_type]
+
+        #since the model type is gpt2 the following hyperparameters
+        model_config['vocab_size']=50257
+        model_config['block_size']=1024
+        
+        #lets make it regular config
+        config=GPTConfig(**model_config)
+        #define the model
+        model=GPT(config)
+
+        #now lets clear some parameters that can't be loaded 
+        sd=model.state_dict()
+        #keys of the model
+        sd_keys=sd.keys()
+    
+        #lets remove keys we dont want
+        sd_keys=[k for k in sd_keys if not k.endwith('.attn.bias')]
+
+        #lets load the model form hugging face
+        from transformers import GPT2LMHeadModel
+        model_hf=GPT2LMHeadModel.from_pretrained(config)
+        sd_hf=model_hf.state_dict()
+        sd_keys_hf=sd_hf.keys()
+
+        #the keys we want are
+        sd_keys_hf=[k for k in sd_keys_hf if not k.endswith('.attn.bias')]
+        sd_keys_hf=[k for k in sd_keys_hf if not k endswith('.attn.masked_bias')]
+        
+        #lets specify values that are transposed
+        transpose=['attn.c_proj.weight','attn.c_attn.weight','mlp.c_fc.weight','mlp.c_proj.weight']
+    
+        assert len(sd_keys_hf)==len(sd_keys),f'the length of the model({len(sd_keys)}) and the blank model({sd_keys_hf}) are difference'
+        #lets copy the parameters
+        for k in sd_keys_hf:
+            if any(k.endswith(i) for i in transpose):
+                #am not still sure why but we need to check for the inverse
+                assert k.shape[::-1]==k.shape
+                with torch.no_grad:
+                    sd_keys[k].copy_(sd_keys_hf[k].t())
+            else:
+                #those that are not conv1D
+                assert sd_keys[k].shape==sd_keys_hf[k].shape
+                with torch.no_grad:
+                    sd_keys[k].copy_(sd_keys_hf[k])
+                    
+        return model
+
 
 class MLP(nn.Module):
     def __init__(self,config):
